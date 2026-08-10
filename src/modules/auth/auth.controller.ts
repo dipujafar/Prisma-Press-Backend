40dc55538002c134b46ed3816c15f81771966fb4
dipujafar <dipujafarmr@gmail.com>
2026-08-10 21:28:15 +0600

    write schema for posts and comments

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,38 +2,65 @@ import { NextFunction, Request, Response } from "express";
 import { catchAsync } from "../../utils/catchAsync";
 import { authService } from "./auth.service";
 import { sendResponse } from "../../lib/sendResponse";
+import httpStatus from "http-status";
 
-const loginUser = catchAsync(async (req:Request, res: Response, next: NextFunction) => {
+const loginUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
     const payload = req.body;
 
-    const {accessToken, refreshToken} = await authService.loginUser(payload);
+    const { accessToken, refreshToken } = await authService.loginUser(payload);
 
     res.cookie("accessToken", accessToken, {
-        httpOnly: true,
-        secure: false,
-        sameSite: "none",
-        maxAge: 24 * 60 * 60 * 24 * 7
-    })
+      httpOnly: true,
+      secure: false,
+      sameSite: "none",
+      maxAge: 24 * 60 * 60 * 24 * 7,
+    });
 
     res.cookie("refreshToken", refreshToken, {
-        httpOnly: true,
-        secure: false,
-        sameSite: "none",
-        maxAge: 24 * 60 * 60 * 24 * 30
-    })
-
+      httpOnly: true,
+      secure: false,
+      sameSite: "none",
+      maxAge: 24 * 60 * 60 * 24 * 30,
+    });
 
     sendResponse(res, {
-        success: true,
-        statusCode: 200,
-        message: "User logged in successfully",
-        data: {
-            accessToken,
-            refreshToken
-        },
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "User logged in successfully",
+      data: {
+        accessToken,
+        refreshToken,
+      },
     });
-});
+  },
+);
+
+const refreshToken = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const refreshToken = req.cookies.refreshToken;
+
+    const { accessToken } = await authService.refreshToken(refreshToken);
+
+    res.cookie("accessToken", accessToken, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "none",
+      maxAge: 24 * 60 * 60 * 24 * 7,
+    });
+
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: "Token refreshed successfully",
+      data: {
+        accessToken,
+      },
+    });
+  },
+);
 
 export const authController = {
-    loginUser,
+  loginUser,
+  refreshToken,
 };
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -3,7 +3,7 @@ import { prisma } from "../../lib/prisma";
 import { ILoginUser } from "./auth.interface";
 import { jwtUtils } from "../../utils/jwt";
 import config from "../../config";
-import { SignOptions } from "jsonwebtoken";
+import { JwtPayload, SignOptions } from "jsonwebtoken";
 
 const loginUser = async (payload: ILoginUser) => {
   const { email, password } = payload;
@@ -47,6 +47,43 @@ const loginUser = async (payload: ILoginUser) => {
   };
 };
 
+const refreshToken = async (refreshToken: string) => {
+  const verifiedRefreshToken = jwtUtils.verifyToken(
+    refreshToken,
+    config.jwt_refresh_secret,
+  );
+
+  if (!verifiedRefreshToken.success) {
+    throw new Error(verifiedRefreshToken.error);
+  }
+
+  const { id } = verifiedRefreshToken.data as JwtPayload;
+
+  const user = await prisma.user.findUniqueOrThrow({
+    where: { id },
+  });
+
+  if (user.activeStatus === "BLOCKED") {
+    throw new Error("Your account has been blocked. Please contact support.");
+  }
+
+  const jwtPayload = {
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    role: user.role,
+  };
+
+  const accessToken = jwtUtils.createToken(
+    jwtPayload,
+    config.jwt_access_secret,
+    config.jwt_access_expires_in as SignOptions,
+  );
+
+  return { accessToken };
+};
+
 export const authService = {
   loginUser,
+  refreshToken,
 };
